d0904024c66d4b8ef27cc61601420508d8bdc360
Igrom <igorsowinski.mail@gmail.com>
2018-06-22 13:55:30 +0200

    Storybook integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,45 +9,40 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const auth = new Auth0("cimpress.auth0.com", CLIENT_ID, CLIENT_SECRET);
 
 let plugin = new CimpressTranslationsWebpackPlugin({
-    serviceId: "7f446c56-8c8e-438b-9cb2-64e5d7b1d89c",
-    path: path.join(__dirname, "./src/locales/translations.json"),
-    authorizer: {
-        getAccessToken: async () => await auth.getAccessTokenUsingRefreshToken()
-    }
+  serviceId: "7f446c56-8c8e-438b-9cb2-64e5d7b1d89c",
+  path: path.join(__dirname, "./src/locales/translations.json"),
+  authorizer: {
+    getAccessToken: async () => await auth.getAccessTokenUsingRefreshToken()
+  }
 });
 
 module.exports = function (grunt) {
 
-    require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('grunt-webpack');
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-webpack');
 
-    grunt.initConfig({
-
-        verbose: true,
-
-        webpack: {
-            config: () => ({
-                plugins: [plugin],
-                resolve: {
-                    extensions: ['.js', '.jsx']
-                },
-                entry: './src',
-                mode: 'development',
-                module: {
-                    rules: [{
-                        test: /\.css$/,
-                        use: [{
-                            loader: 'style-loader'
-                        }, {
-                            loader: 'css-loader',
-                        }]
-                    }, {
-                        test: /\.jsx?$/,
-                        exclude: /node_modules/,
-                        loader: 'babel-loader'
-                    }]
-                }
-            })
+  grunt.initConfig({
+    verbose: true,
+    webpack: {
+      config: () => ({
+        plugins: [plugin],
+        resolve: {
+          extensions: ['.js', '.jsx']
+        },
+        entry: './src',
+        module: {
+          rules: [
+            {
+              test: /\.css$/,
+              use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader', }
+              ]
+            },
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+          ]
         }
-    });
+      })
+    }
+  });
 };
---
+++ stories/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import FulfillerSelect from '../src/FulfillerSelect'
+import { CssLoader } from '@cimpress/react-components'
+
+let wrapInLoader = component => {
+  return (
+    <CssLoader>
+      {component}
+    </CssLoader>
+  );
+};
+
+let newComp = props => {
+  return (<h1>this.props.text</h1>);
+}
+
+let fulfillers = [
+  {
+    name: "My Test Fulfiller",
+    internalFulfillerId: 31337,
+    fulfillerId: "deadbeef",
+    archived: false
+  },
+  {
+    name: "My Test Fulfiller 2",
+    internalFulfillerId: 31338,
+    fulfillerId: "deadbef0",
+    archived: false
+  },
+  {
+    name: "My Test Fulfiller 3",
+    internalFulfillerId: 31339,
+    fulfillerId: "deadbef1",
+    archived: false
+  }
+];
+
+storiesOf('Fulfiller Selection', module)
+  .add('with everything included', () => wrapInLoader(
+    <FulfillerSelect
+      language={'eng'}
+      fulfillers={fulfillers}
+      onChange={v => {}}
+      includeArchived={true}
+      includeId={true}
+      includeInternalId={true}
+      includeName={true}
+    />
+  ));
